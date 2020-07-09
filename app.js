3e17123fa4a172076c66191335dfd3744c053bb1
jackie <mvpbremah@yahoo.com>
2020-07-08 23:06:54 -0400

    commit

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ function generate(){
 
     // set password length/complexity
     let complexity = document.getElementById("slider").value;
-    
+
+    // possible values.
+let values = "abcdefghijklmnopqrstuvwxyz";
+let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let values = "1234567890";
+let values = `!"#$%&'(\`)*+,-./:;<=>?@[\\]^_{|}~`;
+
+let password= "";
+
+// create for loop to choose password characters
+
+for( var i=0; i <= complexity; i++);
+  password = password + values.charAt(Math.floor(Math.random() * Math.floor(Value.length - 1)));
 
 
 }
+
+   //add password to textbox/diplay area
+   document.getElementById("display").values = password;
